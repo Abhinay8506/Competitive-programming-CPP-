2aeae41fabf1588da511430e17a528187b5185d5
Abhinay Goswami <abhinay.21GCEBAIDS059@galgotiacollege.edu>
2023-03-05 04:32:56 -0800

    Add files via upload

---
+++ prac1.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+/*inline void sum(int);*/
+/*class set
+{
+    public:
+       inline int square(int a);
+};*/
+/*class set
+{
+    public:
+       inline int square(int a){
+          return (a*a); 
+       }
+};*/
+
+
+/*int sum(int);*/
+int main(){
+    int a;
+    set S;
+    /*cout<<a;
+    cout<<a<<endl<<a<<endl<<a;
+    cout<<"ram"<<"\n"<<a<<endl<<a<<"\t"<<a;*/
+    /*
+    cin>>a>>b>>c;
+    cout<<a<<endl<<b<<endl<<c<<endl; */
+    /*cin>>ch;
+    while(1){
+        
+        switch(ch){
+         case 1:printf("ram");
+                continue;
+         case 2:printf("shyam");
+                
+         case 3:printf("abhi");
+         default:printf("invalid");
+                 
+    }}*/
+    /*
+    cin>>a;
+    for(i=0;i<=a;i++){
+        cout<<sum(i)<<"\t";
+    }*/
+    /*cin>>a>>b;
+    sum(a,b);
+    cout<<"ramnarayan"<<endl;*/
+    /*cin>>a;
+    cout<<S.square(a)<<endl;*/
+}
+/*int sum(int n){
+    if(n==1||n==0){
+        return n;
+    }
+    
+    return sum(n-2)+sum(n-1);
+    
+}*/
+/*
+inline void sum(int a,int b){
+    cout<<(a+b)<<endl;
+}*/
+/*inline int set::square(int a){
+    return (a*a);
+}*/
---
+++ prac2.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   /* int l,b;
+    cin>>l>>b;
+    cout<<l*1LL*b<<endl;*/
+    
+    string b="hello";
+    cout<<b;
+    
+    return 0;
+}
