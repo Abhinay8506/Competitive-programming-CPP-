464c71da6b56aba6738a47f14ea4dd1935523375
Abhinay Goswami <abhinay.21GCEBAIDS059@galgotiacollege.edu>
2023-03-06 15:52:27 +0530

    Update prac2.cpp

--- prac2.cpp
+++ prac2.cpp
@@ -4,9 +4,41 @@ int main(){
    /* int l,b;
     cin>>l>>b;
     cout<<l*1LL*b<<endl;*/
-    
+    /*string a='hello';
+    cout<<a<<endl;---error
     string b="hello";
-    cout<<b;
+    cout<<b;*/
+    /*int t;
     
+    cin>>t;
+    while(t--){
+        string str;
+        getline(cin,str);
+        cout<<str<<endl;
+    }*/
+    /*int t1,t2;
+    string str;
+    cin>>t1;
+    cin>>t2;
+    getline(cin,str);
+    cout<<endl<<str;-----reading---spaces*/
+    /*int t;
+    cin>>t;
+    try{
+        if(t>0){
+            cout<<"before throw"<<endl;
+            throw 10;
+            cout<<"after throw"<<endl;
+        }
+        else{
+            throw 'a';
+            cout<<"else part"<<endl;
+        }
+    }
+    catch(...){
+        cout<<"in catch"<<endl;
+
+    }
+    cout<<"ending"<<endl; */
     return 0;
 }
