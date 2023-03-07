459f1b83c776ca613f5dfc8bd092d7be3b44c0c4
Abhinay Goswami <abhinay.21GCEBAIDS059@galgotiacollege.edu>
2023-03-07 13:30:16 +0530

    Create prac4.cpp

---
+++ prac4.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*char upper(char c){
+	return ('A'+ c-'a');
+}*/
+int main(){
+	
+	/*while(true){
+		string str;
+		cin>>str;
+		if(str.size()==0)
+		{
+			break;
+		}
+		for(int i=0;i<str.size();i++){
+			str[i]=upper(str[i]);
+		}
+		cout<<str<<endl;
+	}*/
+  
+  /*int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		int arr[n];
+		for(int i=0;i<n;i++){
+			cin>>arr[i];
+		}
+		long long int pro=1;
+		for(int i=0;i<n;i++){
+			pro=pro*arr[i];
+		}
+		int c=(pro%10);
+		if(c==2||c==3||c==5)
+		{
+			cout<<"YES"<<endl;
+		}
+		else{
+			cout<<"NO"<<endl;
+		}
+	}*/
+  
+ /* int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		string str;
+		cin>>str;
+		long int sum=0;
+		long power=1;
+		for(int i=(str.size()-1);i>=0;i--){
+            int bin=str[i]-'0';
+			sum=sum+(bin*power);
+			power=power*2;
+		}
+		cout<<sum<<endl;
+	}*/
+  
+ /* int t;
+	cin>>t;
+	while(t--){
+		long long int n;
+		cin>>n;
+		cout<<(4*n)<<endl;
+	}*/
+
+	}
+	
