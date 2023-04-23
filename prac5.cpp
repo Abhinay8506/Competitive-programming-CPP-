8b30f8534793ad668f0019f83582159dfc984aac
Abhinay Goswami <abhinay.21GCEBAIDS059@galgotiacollege.edu>
2023-04-23 16:08:10 +0530

    Update prac5.cpp

--- prac5.cpp
+++ prac5.cpp
@@ -41,7 +41,7 @@ int main(){
 		long long int a[n][n];
 		a[0][0]=1;
 		for(int i=1;i<n;i++){
-             a[i][0]=1;
+                         a[i][0]=1;
 			 a[i][i]=1;
 			 for(int j=1;j<i;j++){
 				 a[i][j]=a[i-1][j-1]+a[i-1][j];
